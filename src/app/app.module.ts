7ba9ea6fb21bdfceb237d1b508a00dd1fb797e9e
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-02-13 13:56:27 +0100

    testing of angular basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    Test2Component,
+    Test3Component
   ],
   imports: [
     BrowserModule,
