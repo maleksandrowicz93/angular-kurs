cda994f310349bc8d7a8b0f4b53556a988e2b198
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-02-16 08:55:27 +0100

    add two way binding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { PropertBindingComponent } from './propert-binding/propert-binding.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
 import { PropertyEventBindingComponent } from './property-event-binding/property-event-binding.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertBindingComponent,
     EventBindingComponent,
-    PropertyEventBindingComponent
+    PropertyEventBindingComponent,
+    TwoWayBindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
