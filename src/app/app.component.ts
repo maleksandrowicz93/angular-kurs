adf410a3ef4ce7e9dca546eb7c5a96c4af2ce228
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-02-17 13:25:08 +0100

    code refactoring with the use of components interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  newTask: string;
   tasksList: Array<string> = [];
   tasksDone: Array<string> = [];
 
-  add() {
-    this.tasksList.push(this.newTask);
-    this.newTask = '';
-    console.log(this.tasksList);
+  add(task: string) {
+    this.tasksList.push(task);
   }
 
   remove(task: string) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { TodoTaskComponent } from './todo-task/todo-task.component';
+import { DoneTaskComponent } from './done-task/done-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    TodoTaskComponent,
+    DoneTaskComponent
   ],
   imports: [
     BrowserModule,
