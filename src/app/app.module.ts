2d387b0a430aaf4c5068802a6014c305a6b4a2d8
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-02-17 15:09:14 +0100

    add content projection components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
+import { ContentProjectionComponent } from './content-projection/content-projection.component';
+import { ContentProjection2Component } from './content-projection2/content-projection2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    ContentProjectionComponent,
+    ContentProjection2Component
   ],
   imports: [
     BrowserModule,
